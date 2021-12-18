f6dd489a1d05ef0b6a065868d563d96bc4b90b5c
Mattia Denegri <mattia.denegri@gmail.com>
2021-12-18 16:12:09 +0100

    esercizio calcolo reazione terminato

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-12">
+      <h1 class="display-1 px-5">Valuta i tuoi tempi di reazione</h1>
+    </div>
+    <div class="col-10">
+      <p class="fs-5">clicca sul bottone, dopo un intervallo di tempo casuale apparirá un rettangolo verde, cliccaci sopra piu veloce che puoi e calcola la tua velocitá di reazione!!!</p>
+  <button class="my-3 btn btn-success" @click="start" :disabled="isPlaying">play</button>
+  <block v-if="isPlaying" :delay="delay" @end='endGame'/>
+  <results v-if="showResults" :score='score'/>
+    </div>
+  </div>
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/block.vue'
+import results from './components/results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, results },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null,
+      score: null,
+      showResults:false,
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random()*5000
+      this.isPlaying = true
+      this.showResults= false
+    },
+    endGame(reactionTime){
+      this.score= reactionTime
+      this.isPlaying= false
+      this.showResults=true
+    }
   }
 }
 </script>
@@ -20,7 +52,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #333;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/results.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>Reaction time: {{score}}ms </p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+ props:['score'],
+ data(){
+     return{
+         rank:null
+     }
+ },
+ mounted(){
+     if(this.score < 270){
+         this.rank = 'ottimi riflessi'
+     }else if(this.score < 450){
+         this.rank = 'buoni riflessi'
+     }else{this.rank = 'devi allenarti'}
+ }
+}
+</script>
+
+<style>
+.rank{
+    color: black;
+    font-weight: bold;
+    font-size: 20px;
+}
+</style>
